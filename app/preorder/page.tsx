6359be886dfc992a3c73ae07e16794a80d9fb767
Avalynn dev <avalynndev@gmail.com>
2023-02-27 05:24:35 +0000

    added preorder feature

---
+++ app/preorder/page.tsx
@@ -0,0 +1,172 @@
+"use client"
+import { useState } from "react";
+import { sendContactForm } from "../../lib/api";
+interface FormValues {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
+
+interface FormState {
+  isLoading: boolean;
+  error: string;
+  values: FormValues;
+}
+const initValues: FormValues = {
+  name: "",
+  email: "",
+  subject: "",
+  message: "",
+};
+
+const initState: FormState = {
+  isLoading: false,
+  error: "",
+  values: initValues,
+};
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Text,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import { Heading, Container, extendTheme } from "@chakra-ui/react";
+
+
+export default function Home() {
+  const toast = useToast();
+  const [state, setState] = useState<FormState>(initState);
+  const [touched, setTouched] = useState<{ [key: string]: boolean }>({});
+
+  const { values, isLoading, error } = state;
+
+  const onBlur = ({ target }:any) =>
+    setTouched((prev) => ({ ...prev, [target.name]: true }));
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+    setState((prev) => ({
+      ...prev,
+      values: {
+        ...prev.values,
+        [target.name]: target.value,
+      },
+    }));
+
+  const onSubmit = async () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
+    try {
+      await sendContactForm(values);
+      setTouched({});
+      setState(initState);
+      toast({
+        title: "Message sent.",
+        status: "success",
+        duration: 2000,
+        position: "top",
+      });
+    } catch (error:any) {
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+        error: error.message,
+      }));
+    }
+  };
+
+  return (
+    <CacheProvider>
+        <ChakraProvider theme={extendTheme({ initialColorMode: "dark", })}>
+            <Container textAlign="center" fontSize="2xl" p="1em" maxW="450px" mt={12}>
+            <Heading>Preorder</Heading>
+      {error && (
+        <Text color="red.300" my={4} fontSize="xl">
+          {error}
+        </Text>
+      )}
+                <FormControl isRequired isInvalid={touched.name && !values.name} mb={5}>
+        <FormLabel>Name</FormLabel>
+        <Input
+          type="text"
+          name="name"
+          errorBorderColor="red.300"
+          value={values.name}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        <FormErrorMessage>Required</FormErrorMessage>
+      </FormControl>
+
+      <FormControl isRequired isInvalid={touched.email && !values.email} mb={5}>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          name="email"
+          errorBorderColor="red.300"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        <FormErrorMessage>Required</FormErrorMessage>
+      </FormControl>
+
+      <FormControl
+        mb={5}
+        isRequired
+        isInvalid={touched.subject && !values.subject}
+      >
+        <FormLabel>Subject</FormLabel>
+        <Input
+          type="text"
+          name="subject"
+          errorBorderColor="red.300"
+          value={values.subject}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        <FormErrorMessage>Required</FormErrorMessage>
+      </FormControl>
+
+      <FormControl
+        isRequired
+        isInvalid={touched.message && !values.message}
+        mb={5}
+      >
+        <FormLabel>Message</FormLabel>
+        <Textarea
+          typeof="text"
+          name="message"
+          rows={4}
+          errorBorderColor="red.300"
+          value={values.message}
+          onChange={handleChange}
+          onBlur={onBlur}
+        />
+        <FormErrorMessage>Required</FormErrorMessage>
+      </FormControl>
+
+      <Button
+        variant="outline"
+        colorScheme="blue"
+        isLoading={isLoading}
+        disabled={
+          !values.name || !values.email || !values.subject || !values.message
+        }
+        onClick={onSubmit}
+      >
+        Submit
+          </Button>
+            </Container>
+        </ChakraProvider>
+    </CacheProvider>
+  )
+};
